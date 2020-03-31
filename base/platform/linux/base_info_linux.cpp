b2756438d536b4cfc077b6dfeca6bd62a40c8cd9
Henry Hu <henry.hu.sh@gmail.com>
2020-03-30 20:57:58 -0400

    change system version

--- base/platform/linux/base_info_linux.cpp
+++ base/platform/linux/base_info_linux.cpp
@@ -65,7 +65,7 @@ QString SystemVersionPretty() {
 	const auto result = getenv("XDG_CURRENT_DESKTOP");
 	const auto value = result ? QString::fromLatin1(result) : QString();
 	const auto list = value.split(':', QString::SkipEmptyParts);
-	return list.isEmpty() ? "Linux" : "Linux " + list[0];
+	return list.isEmpty() ? "FreeBSD" : "FreeBSD " + list[0];
 }
 
 QString SystemCountry() {
